04ade094b2e9620bce7231b956ef7f4224a4e7f2
Rajendra N <rajendra.namdeo@adapty.com>
2018-02-02 12:39:10 +0530

    frontends Subscription functionality changes

---
+++ app/code/local/Thycart/Subscription/Block/SubscriptionList.php
@@ -0,0 +1,11 @@
+<?php
+	
+class Thycart_Subscription_Block_SubscriptionList extends Mage_Core_Block_Template
+{
+
+	public function subscriptionList()
+	{
+		die('test');
+	}
+
+}
---
+++ app/code/local/Thycart/Subscription/controllers/IndexController.php
@@ -0,0 +1,8 @@
+<?php
+class Thycart_Subscription_IndexController extends Mage_Core_Controller_Front_Action
+{
+	protected function subscriptionAction()
+	{
+		die('in block');
+	}
+}
