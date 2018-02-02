b08059573af77d7442b36cdb6600c8ae47c5b721
Ritesh Shukla <ritesh.shukla@adapty.com>
2018-02-02 13:41:28 +0530

    button and jquery to for subscription checkout

--- app/code/local/Thycart/Subscription/controllers/IndexController.php
+++ app/code/local/Thycart/Subscription/controllers/IndexController.php
@@ -6,4 +6,8 @@ class Thycart_Subscription_IndexController extends Mage_Core_Controller_Front_Ac
 		$this->loadLayout();
 		$this->renderLayout();
 	}
+	public function checkoutAction()
+	{
+		
+	}
 }
