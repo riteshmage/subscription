d19e2f5af496727d5d1aeb7a6acb14cef927a873
Rajendra N <rajendra.namdeo@adapty.com>
2018-02-12 13:02:22 +0530

    subscription cancellation and order cancellation

--- app/code/local/Thycart/Subscription/controllers/IndexController.php
+++ app/code/local/Thycart/Subscription/controllers/IndexController.php
@@ -11,31 +11,73 @@ class Thycart_Subscription_IndexController extends Mage_Core_Controller_Front_Ac
 		$this->loadLayout();
 		$this->renderLayout();
 	}
+    
 	public function cancelAction()
     {
-    	if(empty($this->getRequest()->getParam('increment_id')))
+    	if(empty($this->getRequest()->getParam('order_number')) || empty($this->getRequest()->getParam('subscription_id')))
     	{
     		 Mage::getSingleton('core/session')->addError('Order Id is wrong');
     		 $this->_redirect('subscription/index/subscription');
     		 return;
     	}
-        $incrementId = $this->getRequest()->getParam('increment_id');  		
-        try
-        {	
-        	$order = Mage::getModel('sales/order')->load($incrementId, 'increment_id');
-            $order->cancel();
-            $order->save();
-            $order->sendOrderUpdateEmail();
-            Mage::getSingleton('core/session')->addSuccess('Order Cancelled Sucessfully');
-        }
-        catch(Exception $e)
+        $orderNumber = $this->getRequest()->getParam('order_number');
+        $subscriptionId = $this->getRequest()->getParam('subscription_id');
+        $order = Mage::getModel('sales/order')->load($orderNumber, 'increment_id');
+
+        $subscriptionCancelled = $this->cancelSubscription($subscriptionId);
+        if(strtolower($order->getStatusLabel()) === 'pending')
         {
-            Mage::logException($e);
-            Mage::getSingleton('core/session')->addError('Order cannot be canceled.');
+            $orderCancelled = $this->cancelOrder($order);
+        }
+        if($subscriptionCancelled)
+        {            
+            Mage::getSingleton('core/session')->addSuccess('Subscription Cancelled Sucessfully');
+        }
+        else
+        {
+            Mage::getSingleton('core/session')->addError('Subscription cannot be canceled');
             exit;
         }
         $this->_redirect('subscription/index/subscription');
         return;
-        $order->getStatusLabel();
+    }
+
+    public function cancelSubscription($subscriptionId=0)
+    {
+        if(empty($subscriptionId))
+        {
+            return;
+        }
+        $subscriptionModel = Mage::getModel('subscription/subscriptioncustomer')->load($subscriptionId);
+        $data = array('active'=>0);
+        try
+        {
+            $subscriptionModel->addData($data);
+            $subscriptionModel->save();
+            return true;
+        }
+        catch(Exception $e)
+        {
+            throw new Exception(EXCEPTION_MSG);return;            
+        }
+    }
+
+    public function cancelOrder($order)
+    {
+        if(empty($order))
+        {
+            return;
+        }
+        try
+        {
+            $order->cancel();
+            $order->save();
+            $order->sendOrderUpdateEmail();
+            return true;
+        }
+        catch(Exception $e)
+        {
+            throw new Exception(EXCEPTION_MSG);return;            
+        }
     }
 }
