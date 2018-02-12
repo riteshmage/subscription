524b2a2b8bf25bf29787730d345826136ebfcbce
Ritesh Shukla <ritesh.shukla@adapty.com>
2018-02-12 11:19:31 +0530

    allow user to cancel subscription

--- app/code/local/Thycart/Subscription/controllers/IndexController.php
+++ app/code/local/Thycart/Subscription/controllers/IndexController.php
@@ -1,7 +1,7 @@
 <?php
 class Thycart_Subscription_IndexController extends Mage_Core_Controller_Front_Action
 {
-	protected function subscriptionAction()
+	public function subscriptionAction()
 	{
 		if(!Mage::getSingleton('customer/session')->isLoggedIn()) 
 		{
@@ -11,4 +11,31 @@ class Thycart_Subscription_IndexController extends Mage_Core_Controller_Front_Ac
 		$this->loadLayout();
 		$this->renderLayout();
 	}
+	public function cancelAction()
+    {
+    	if(empty($this->getRequest()->getParam('increment_id')))
+    	{
+    		 Mage::getSingleton('core/session')->addError('Order Id is wrong');
+    		 $this->_redirect('subscription/index/subscription');
+    		 return;
+    	}
+        $incrementId = $this->getRequest()->getParam('increment_id');  		
+        try
+        {	
+        	$order = Mage::getModel('sales/order')->load($incrementId, 'increment_id');
+            $order->cancel();
+            $order->save();
+            $order->sendOrderUpdateEmail();
+            Mage::getSingleton('core/session')->addSuccess('Order Cancelled Sucessfully');
+        }
+        catch(Exception $e)
+        {
+            Mage::logException($e);
+            Mage::getSingleton('core/session')->addError('Order cannot be canceled.');
+            exit;
+        }
+        $this->_redirect('subscription/index/subscription');
+        return;
+        $order->getStatusLabel();
+    }
 }
