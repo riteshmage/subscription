e3f973b17c66b8db21c1e0fb34a700ae1178d78e
Ritesh Shukla <ritesh.shukla@adapty.com>
2018-02-19 23:39:20 +0530

    added validation functions

--- app/code/local/Thycart/Subscription/Helper/Data.php
+++ app/code/local/Thycart/Subscription/Helper/Data.php
@@ -15,7 +15,49 @@ class Thycart_Subscription_Helper_Data extends Mage_Core_Helper_Abstract
 		$finalArr['value'] = $units;
 		$finalArr['label'] = 'Please Select Unit';
 		return $finalArr;
+	}
 
+	public function isNumber($var)
+	{
+		if(is_numeric($var))
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public function isAlphanum($var)
+	{
+		$validator = new Zend_Validate_Alnum(array('allowWhiteSpace' => true));
+		if($validator->isValid($var))
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public function numericRange($num, $min, $max)
+	{
+		$validator  = new Zend_Validate_Between(array('min' => $min, 'max' => $max));
+		if($validator->isValid($num))
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public function validateData($postData=array())
+	{
+		foreach ($postData as $key => $value)
+		{
+			if(is_array($value))
+			{
+				continue;
+			}
+			$data = trim($value);
+			$data = Mage::helper('core')->escapeHtml($data);
+			$postData[$key] =$data;
+		}
+		return $postData;
 	}
 }
-
