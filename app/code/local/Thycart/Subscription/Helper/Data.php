25a2e45df3fa5f55a6e4b03b33731f8878955e2f
Ritesh Shukla <ritesh.shukla@adapty.com>
2018-02-24 22:45:13 +0530

    added new email validation, email function and getEmailBody functions

--- app/code/local/Thycart/Subscription/Helper/Data.php
+++ app/code/local/Thycart/Subscription/Helper/Data.php
@@ -1,4 +1,5 @@
 <?php
+require_once('phpmailer/class.phpmailer.php');
 class Thycart_Subscription_Helper_Data extends Mage_Core_Helper_Abstract
 {
 	public function getUnitMaster()
@@ -59,7 +60,7 @@ class Thycart_Subscription_Helper_Data extends Mage_Core_Helper_Abstract
 		return false;
 	}
 
-	public function validateData($postData=array())
+	public function validateData($postData)
 	{
 		foreach ($postData as $key => $value)
 		{
@@ -73,4 +74,164 @@ class Thycart_Subscription_Helper_Data extends Mage_Core_Helper_Abstract
 		}
 		return $postData;
 	}
+
+	public function isEmail($email)
+	{
+		if (filter_var($email, FILTER_VALIDATE_EMAIL))
+		{
+  			return true;
+		}
+		return false;
+	}
+	public function isAlpha($var)
+	{
+		if (preg_match("/^[a-zA-Z ]*$/",$var))
+		{
+  			return true;
+		}
+		return false;
+	}
+	public function sendMail($to, $recepientName, $subject, $status, $productName ,$unit)
+    {
+        if(empty($to) || empty($recepientName) || empty($subject) ||empty($status) || empty($productName) || empty($unit))
+        {
+            return false;
+        }
+        if(!$this->isEmail($to) || !$this->isAlpha($recepientName) || !$this->isAlpha($status))
+        {
+        	return false;
+        }
+        if(PHP_MAILER)
+        { 
+            try
+            {
+                $mail = new PHPMailer();
+                $mail->IsSMTP();
+                $mail->SMTPDebug = 1; 
+                $mail->SMTPAuth = true;
+                $mail->SMTPSecure = 'ssl';
+                $mail->Host = "smtp.gmail.com";
+                $mail->Port = 465;
+                $mail->IsHTML(true);
+                $mail->Username = Mage::getStoreConfig('notification/email_group/sender_email');
+                $mail->Password = '';
+                $mail->SetFrom(Mage::getStoreConfig('notification/email_group/sender_email'));
+                $mail->Subject = $subject;
+                $mail->Body = Mage::helper('subscription')->getEmailBody($status, $recepientName, $productName ,$unit);
+                $mail->AddAddress($to);
+            }
+            catch (Exception $e)
+            {
+                echo $e->getMessage();
+                return false;
+            }
+            
+            if(!$mail->Send()) 
+            {
+               return false;
+            }   
+        }
+        if(MAGENTO_MODEL)
+        {
+            $mail = Mage::getModel('core/email');
+            $mail->setToName($recepientName);
+            $mail->setToEmail($to);
+            $mail->setBody(Mage::helper('subscription')->getEmailBody($status, $recepientName, $productName ,$unit));
+            $mail->setSubject($subject);
+            $mail->setFromEmail(Mage::getStoreConfig('notification/email_group/sender_email'));
+            $mail->setType('html');
+            try
+            {
+                if(!$mail->send())
+                {
+                    return false;
+                }
+            }
+            catch (Exception $e)
+            {
+                Mage::getSingleton('core/session')->addError('Unable to send  Email.');
+                return false;
+            }   
+        }
+        if(ZEND_FUNCTION)
+        {
+            $sender_email = Mage::getStoreConfig('notification/email_group/sender_email');
+            $sender_name  = Mage::getStoreConfig('notification/email_group/sender_name');
+
+            $mail = new Zend_Mail();
+            $mail->setBodyHtml(Mage::helper('subscription')->getEmailBody($status, $recepientName, $productName ,$unit)); 
+            $mail->setFrom($sender_email,$sender_name);
+            $mail->addTo($to, 'customer');
+            //$mail->addCc($cc, $ccname);    //can set cc
+            //$mail->addBCc($bcc, $bccname);    //can set bcc
+            $mail->setSubject($subject);
+            try
+            {
+                if(!$mail->send())
+                {
+                    return false;
+                }
+            }
+            catch(Exception $ex)
+            {
+                echo $e->getMessage();
+                return false;
+            }
+        }
+        if(Mage::getStoreConfig('notification/email_group/email'))
+        {
+            $templateId     = 1;     
+            $senderName     = Mage::getStoreConfig('notification/email_group/sender_name');
+            $senderEmail    = Mage::getStoreConfig('notification/email_group/sender_email');        
+            $sender         = array('name' => $senderName,
+                                    'email' => $senderEmail
+                                   );              
+            $storeId = Mage::app()->getStore()->getId();
+            $vars    = array('status' => $status,
+                             'customerName'=> $recepientName,
+                             'productName'=>$productName,
+                             'unit'=>$unit
+                            );
+            try
+            {
+                $translate  = Mage::getSingleton('core/translate');
+                Mage::getModel('core/email_template')
+                        ->sendTransactional($templateId, $sender, $to, $recepientName, $vars, $storeId);    
+                $translate->setTranslateInline(true);   
+            }
+            catch (Exception $e)
+            {
+                echo $e->getMessage();
+                return false;
+            }
+        }     
+        return true;
+    }
+
+    public function getEmailBody($status, $recepientName, $productName ,$unit)
+    { 
+        $body ='';
+        $emailTemplateVars = array();
+        if(empty($status) || empty($productName))
+        {
+            return false;
+        }
+       
+        $emailTemplateVars['customerName'] = $recepientName;
+        $emailTemplateVars['productName']  = $productName;
+        $emailTemplateVars['status'] 	   = $status;
+        $emailTemplateVars['unit']		   = $unit;
+        try
+        {
+            $emailTemplate  = Mage::getModel('core/email_template')
+                            ->loadDefault('subscription_template');
+            $body = $emailTemplate->getProcessedTemplate($emailTemplateVars);
+        }
+        catch(Exception $e)
+        {
+           echo $e->getMessage(); 
+           return;
+        }
+       return $body;
+    }	
 }
